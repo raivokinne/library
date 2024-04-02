0c33250f50ab92e05917c75a2b241bd060623ef6
RaivoKinne <raivokinne236@gmail.com>
2024-04-02 09:06:09 +0300

    Inital Commit

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -9,7 +9,7 @@ export default function BookList({ book }: Props) {
         <>
             <article
                 key={Math.random()}
-                className="p-6 mb-4 grid place-items-center bg-white rounded-lg shadow-md w-[500px] border-2 border-gray-200 gap-4 h-[900px]"
+                className="p-6 mb-4 grid place-items-center bg-white rounded-lg shadow-md w-[300px] border-2 border-gray-200 gap-4 h-[700px] lg:w-[500px] lg:h-[900px]"
             >
                 <h2 className="mb-2 text-2xl font-bold">{book.title}</h2>
                 <h3 className="mb-2 text-lg font-semibold">{book.name}</h3>
